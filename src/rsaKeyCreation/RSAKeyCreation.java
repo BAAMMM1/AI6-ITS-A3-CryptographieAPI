47771d839406987af4dc1f0af0700119fbddf05c
BAAMMM1 <BAAMMM@outlook.de>
2017-05-15 22:16:04 +0200

    RSAKeyCreation Struktur

--- src/rsaKeyCreation/RSAKeyCreation.java
+++ src/rsaKeyCreation/RSAKeyCreation.java
@@ -1,9 +1,62 @@
 package rsaKeyCreation;
 
-public class RSAKeyCreation {
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
+
+import utility.IOController;
+
+/*
+ * Kann das hier eine SchluesselpaarFactory werden? bzw ein singleton?
+ */
+/**
+ * Diese Klasse erzeugt ein RSA-Schluesselpaar (Schluessellaenge: 2048 Bit),
+ * welche in einer Datei gespeichert werden. Der Name des Inhaber des
+ * Schlüsselpaares wird in der Kommandozeile mit übergeben.
+ * 
+ */
+public class RSAKeyCreation {	
+	
+	private IOController controller;
+	private final static String PATH = "/keys/";
+	private final static String FILE_EXTENTION_PUBIC = ".pub";
+	private final static String FILE_EXTENTION_PRIVATE = ".prv";
 
 	public RSAKeyCreation() {
-		// TODO Auto-generated constructor stub
+		this.controller = new IOController();
 	}
+	
+	
+	public void createKeyPeer(String owner){
+		
+		this.createPublicKey(owner);
+		this.createPrivateKey(owner);
+		
+	}
+	
+	// private Key
+	/*
+	 * Hier entstehen byte array, die als Datei über den IOController geschrieben werden
+	 */
+	private void createPublicKey(String owner){
+		List<byte[]> toWriteLis = new LinkedList<byte[]>();
+		
+		byte[] lenghtOwner = ByteBuffer.allocate(4).putInt(owner.length()).array();
+		byte[] ownerName = owner.getBytes();
+		
+		
+		this.controller.writeFileBlockList(toWriteLis, PATH + owner + FILE_EXTENTION_PUBIC);
+		
+	}
+	
+	private void createPrivateKey(String owner){
+		List<byte[]> toWriteLis = new LinkedList<byte[]>();
+		
+		
+		this.controller.writeFileBlockList(toWriteLis, PATH + owner + FILE_EXTENTION_PRIVATE);
+		
+	}
+	
+	
 
 }
--- src/utility/IOController.java
+++ src/utility/IOController.java
@@ -12,50 +12,58 @@ import java.util.List;
 
 public class IOController {
 
-	InputStream inBlockStream;
-
 	private static final int BLOCK_SIZE = 8;
 
-	public IOController(String KeyPath) throws FileNotFoundException {
-		inBlockStream = new FileInputStream(KeyPath);
+	public IOController() {
 	}
 
-	public LinkedList<byte[]> readFileInBlockList(String path) throws FileNotFoundException, IOException {
+	public LinkedList<byte[]> readFileInBlockList(String path) {
 		LinkedList<byte[]> toReturn = new LinkedList<byte[]>();
 
-		InputStream inStream = new FileInputStream(path);
+		InputStream inStream;
 
-		while (inStream.available() != 0) {
-			// byte[] buffer = new byte[BLOCK_SIZE];
-			byte[] buffer;
+		try {
+			inStream = new FileInputStream(path);
 
-			buffer = new byte[BLOCK_SIZE];
+			while (inStream.available() != 0) {
+				byte[] buffer;
 
-			inStream.read(buffer);
-			toReturn.add(buffer);
+				buffer = new byte[BLOCK_SIZE];
+
+				inStream.read(buffer);
+				toReturn.add(buffer);
+			}
+			inStream.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
-		inStream.close();
-
 		return toReturn;
 	}
 
-	public void writeFileBlockList(List<byte[]> byteArrayList, String path) throws FileNotFoundException, IOException {
-		OutputStream outStream = new FileOutputStream(path);
+	public void writeFileBlockList(List<byte[]> byteArrayList, String path) {
+		OutputStream outStream;
+		try {
+			outStream = new FileOutputStream(path);
 
-		byte[] toWriteBytes = new byte[byteArrayList.size() * BLOCK_SIZE];
+			byte[] toWriteBytes = new byte[byteArrayList.size() * BLOCK_SIZE];
 
-		int byteIndex = 0;
+			int byteIndex = 0;
 
-		for (byte[] byteArray : byteArrayList) {
-			
-			for (int index = 0; index < byteArray.length; index++) {
-				toWriteBytes[byteIndex++] = byteArray[index];
+			for (byte[] byteArray : byteArrayList) {
+
+				for (int index = 0; index < byteArray.length; index++) {
+					toWriteBytes[byteIndex++] = byteArray[index];
+				}
 			}
+
+			outStream.write(toWriteBytes);
+			outStream.close();
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 
-		outStream.write(toWriteBytes);
-		outStream.close();
 	}
 
 }
